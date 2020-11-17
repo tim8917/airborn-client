2d613a091cf57178d39122bbcbe1e637c440bb61
Timur Aminev <timur.aminev@oracle.com>
2020-11-18 01:37:42 +0200

    sendCommand request http --> graphql.

--- src/api/graphql/send-command.js
+++ src/api/graphql/send-command.js
@@ -1,9 +1,11 @@
 export const sendCommand = async ({command}) => {
-    return await fetch('http://localhost:4000/api/http/command', {
+    return await fetch('http://localhost:4000/api/ql', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({command})
+        body: JSON.stringify({
+            query: `{sendCommand(command: "${command}")}`
+        })
     });
 };
